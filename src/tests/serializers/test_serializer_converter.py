c09de4dc3ded1ed1754289ae3069eb354a1b6297
rj3316 <i.usunariz.lopez@gmail.com>
2022-10-03 21:12:43 +0200

    Add converter JSON serializer tests

---
+++ src/tests/serializers/test_serializer_converter.py
@@ -0,0 +1,40 @@
+import json
+import uuid
+
+from ...domain.domainfactory import DomainFactory
+from ...serializers.factoryserializer import SerializerFactory
+from ...serializers.converter import ConverterJsonEncoder
+from ...simulators.factorysimulator import FactorySimulator
+
+def test_room_model_serializer():
+    domain = 'converter'
+
+    # Obtenemos la plantilla sobre la que crearemos la entidad de dominio room
+    sim_dom = FactorySimulator.create_domain_dicts(domain)[0]
+
+    # Instanciamos room, usando sim_dom de plantilla
+    dom = DomainFactory.create(domain, sim_dom)
+
+    # Construimos lo que esperamos que nos devuelva, suando sim_dom de plantilla
+    serializer = SerializerFactory.create(domain)
+
+    json_expected = f"{{"
+    for i, (field, value) in enumerate(sim_dom.items()):
+        is_str = isinstance(value, str)
+
+        json_expected += f"\"{field}\": "
+
+        if is_str: json_expected += "\""
+        json_expected += f"{value}"
+        if is_str: json_expected += "\""
+        if (i+1) != len(sim_dom.keys()): json_expected += ', '
+    json_expected += f"}}"
+    
+    json_dom = json.dumps(dom, cls = serializer)
+
+
+    # Comparamos las dos estructuras
+    dict_json_dom = json.loads(json_dom)
+    dict_json_expected = json.loads(json_expected)
+
+    assert dict_json_dom == dict_json_expected
